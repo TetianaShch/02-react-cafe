10204f4bc70f4b35b878c8b3e4174865c38d9e55
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2025-11-29 00:07:01 +0200

    create component folders and base files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <h1>Sip Happens Café</h1>
+      <p>Please rate our service by selecting one of the options below.</p>
+    </div>
+  );
+}
+
+export default App;
+
